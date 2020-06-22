e1d3c6d26130c9b0e8b2e6ee198faa619a8b3cf0
julio243 <fernandoflo993@gmail.com>
2020-06-22 17:38:36 -0500

    modificado

--- mydb.py
+++ mydb.py
@@ -2,8 +2,10 @@ import mysql.connector
 cnx = mysql.connector.connect(user='root', password = 'julio243', database ='sakila')
 cursor = cnx.cursor()
 #insertar datos
+a=input("inserte nombre: ")
+b=input("inserte apellido: ")
 sql = ("INSERT INTO actor (first_name,last_name) VALUES (%s,%s)")
-data = ("JULIO","FLORES")
+data = (a,b)
 cursor.execute(sql,data)
 cnx.commit()
 #mostrar datos
